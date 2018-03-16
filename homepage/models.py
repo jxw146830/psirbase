0c14cc16e0bc86c7bb47b6534061764041203233
Jamal <whitehead.jamal@gmail.com>
2018-03-15 21:17:54 -0500

    hey

--- homepage/models.py
+++ homepage/models.py
@@ -4,20 +4,21 @@ DEFAULT_SOURCE_ID = 1
 DEFAULT_PLACEHOLDER = "N/A"
 
 class CelegansSirna(models.Model):
-    sequence = models.CharField(max_length=45)
-    name = models.CharField(max_length=50)
+    sequence = models.CharField(max_length=50)
+    name = models.CharField(max_length=30)
     def __str__(self):
         return self.name
-'''
+
 class CelegansBed(models.Model):
-    chr_num = models.CharField(max_length=50, default=DEFAULT_PLACEHOLDER)
-    startPos = models.CharField(max_length=20, default=DEFAULT_PLACEHOLDER)
-    endPos = models.CharField(max_length=20, default=DEFAULT_PLACEHOLDER)
-    sirname = models.CharField(max_length=50, default=DEFAULT_PLACEHOLDER)
-    sign = models.CharField(max_length=1, default="?")
-    author = models.CharField(max_length=100, default=DEFAULT_PLACEHOLDER)
-    stage = models.CharField(max_length=100, default=DEFAULT_PLACEHOLDER)
-    pubmed_id = models.CharField(max_length=20, blank=True, null=True)
+    chr_num = models.CharField(max_length=10, default=DEFAULT_PLACEHOLDER)
+    start = models.CharField(max_length=20, default=DEFAULT_PLACEHOLDER)
+    end = models.CharField(max_length=20, default=DEFAULT_PLACEHOLDER)
+    name = models.CharField(max_length=30, default=DEFAULT_PLACEHOLDER)
+    strand = models.CharField(max_length=1, default="?")
+    stage = models.CharField(max_length=50, default=DEFAULT_PLACEHOLDER)
+    source = models.CharField(max_length=50, default=DEFAULT_PLACEHOLDER)
+    pubmed_id = models.CharField(max_length=12, blank=True, null=True)
+    target_mrna = models.CharField(max_length=50, default=DEFAULT_PLACEHOLDER)
     def __str__(self):
-        return self.author
-'''
+        return self.name
+
