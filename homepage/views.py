cf82c0872e45bcba59f2a1c2b36bd936ab38c520
Jamal <whitehead.jamal@gmail.com>
2018-03-16 02:27:42 -0500

    hey

--- homepage/views.py
+++ homepage/views.py
@@ -109,8 +109,9 @@ def search1(request):
             sirnasResultSet = CelegansSirna.objects.filter(sequence=theSeq)
             for sirnaRow in sirnasResultSet:
                 sirName = sirnaRow.name
+                sirNameLength = len(sirName)
                 #remove > character to get bed file sirna name equivalent
-                sirName = sirName[ 1 : end ]
+                sirName = sirName[ 1 : sirNameLength ]
                 bedRows = CelegansBed.objects.filter(name=sirName)
                 bedFilesResultSet.append(bedRows)
 
